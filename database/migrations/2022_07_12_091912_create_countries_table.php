5ddcb28496c3339b6b0a0d1a9ba517e440c788c9
Monish Khatri <monish.k@biztechcs.com>
2022-07-12 16:54:20 +0530

    add: country,state and city table with data

---
+++ database/migrations/2022_07_12_091912_create_countries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('country_id');
+            $table->string('name');
+            $table->string('iso3');
+            $table->string('iso2');
+            $table->string('phone_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+};
---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Country;
+use File;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Country::truncate();
+
+        $json = File::get("country-state-cities/countries.json");
+        $countries = json_decode($json);
+
+        foreach ($countries as $key => $value) {
+            Country::create([
+                "country_id" => $value->id,
+                "name" => $value->name,
+                "iso3" => $value->iso3,
+                "iso2" => $value->iso2,
+                "phone_code" => $value->phone_code,
+            ]);
+        }
+    }
+}
