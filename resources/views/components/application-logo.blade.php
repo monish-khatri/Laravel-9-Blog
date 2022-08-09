35096e2b0f956c98e831c8623151f07667bb44b9
Monish Khatri <monish.k@biztechcs.com>
2022-08-09 18:41:24 +0530

    Logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="https://www.biztechcs.com/wp-content/themes/biztech/dist/images/logo.svg" width="235" height="46" alt="BiztechCS - Web &amp; Mobile Apps Development Company" title="BiztechCS - Web &amp; Mobile Apps Development Company">
+<img src="{{asset('storage/logo/BiztechcsLogo.png')}}" width="235" height="46" alt="BiztechCS - Web &amp; Mobile Apps Development Company" title="BiztechCS - Web &amp; Mobile Apps Development Company">
