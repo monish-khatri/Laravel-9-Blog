56cdf9923826abe86ca9484b2037e4042e3ebb17
Monish Khatri <monish.k@biztechcs.com>
2022-08-10 16:24:27 +0530

    Job Scheduling

---
+++ app/Console/Commands/BlogCleaner.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Blog;
+use Illuminate\Console\Command;
+
+class BlogCleaner extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'blog:cleaner';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete soft deleted blogs';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return Blog::onlyTrashed()->forceDelete();
+    }
+}
