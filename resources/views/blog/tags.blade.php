ccfe1e31573f224340beda5878b59eb85db87f66
Monish Khatri <khatrimk10@gmail.com>
2022-08-15 18:20:11 +0530

    added mix

--- resources/views/blog/tags.blade.php
+++ resources/views/blog/tags.blade.php
@@ -6,8 +6,6 @@
 </select>
 
 @push('css')
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
     $(document).ready(function () {
         $('#tags').select2({
