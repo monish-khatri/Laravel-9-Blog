94302adf5f05990d470c2b2bac666808759c07f0
Monish Khatri <monish.k@biztechcs.com>
2022-08-16 15:16:24 +0530

    Queue: Send Mail when comment is added

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendEmailJob;
+use App\Mail\SendMailable;
+use App\Models\Blog;
 use App\Models\Comment;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
@@ -32,7 +39,13 @@ class CommentController extends Controller
         $comment->parent_id = $request->parent_id ?? null;
         $comment->blog_id = $request->blog_id;
         $comment->user_id = Auth::id();
-        $comment->save();
+        $result = $comment->save();
+        if($result){
+            // if you want to Send mail after sometime use delay function with carbon
+            $userFrom = User::find($comment->user_id);
+            $blog = Blog::find($comment->blog_id);
+            dispatch(new SendEmailJob($userFrom,$blog->user,$blog,$comment));
+        }
 
         return back();
    }
