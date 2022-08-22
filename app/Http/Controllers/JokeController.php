02cff25417fba75ff78fab04f7091a92814592bf
Monish Khatri <monish.k@biztechcs.com>
2022-08-22 10:57:02 +0530

    Http Request

--- app/Http/Controllers/JokeController.php
+++ app/Http/Controllers/JokeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\HtmlString;
 
 class JokeController extends Controller
@@ -14,25 +15,15 @@ class JokeController extends Controller
    public function randomJoke()
    {
 
-        $curl = curl_init();
-
-        curl_setopt_array($curl, [
-            CURLOPT_URL => "https://v2.jokeapi.dev/joke/Programming,Miscellaneous?type=single",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
+        $response = Http::retry(3, 100)->acceptJson()->get('https://v2.jokeapi.dev/joke/Programming,Miscellaneous', [
+            'type' => 'single',
         ]);
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-        $result = json_decode($response,true);
-        return view('joke.joke', [
-            'joke' => new HtmlString($result['joke']),
-        ]);
+        if($response->ok()) {
+            $result = json_decode($response,true);
+            return view('joke.joke', [
+                'joke' => new HtmlString($result['joke']),
+            ]);
+        }
+        abort($response->status());
    }
 }
